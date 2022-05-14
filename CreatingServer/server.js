1ecbcc75e8d40df59a458d73d71dcff0f6d1528c
nandweza <allannandweza@gmail.com>
2022-05-14 21:48:07 +0300

    working with request and response

--- CreatingServer/server.js
+++ CreatingServer/server.js
@@ -1,7 +1,14 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('Request made!');
+    console.log(req.url, req.method);
+
+//ser header content type
+res.setHeader('Content-Type', 'text/html');
+
+res.write('<p>Hello, Allan</p>');
+res.write('<p>Hello, Allan again!</p>');
+res.end();    
 });
 
 server.listen(3000, 'localhost', () => {
