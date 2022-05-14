d5f72c0c70fe395a8cfe8a953b3d934b86de894b
Nandweza Allan <63627595+nandweza@users.noreply.github.com>
2022-05-14 21:50:46 +0300

    Update server.js

--- CreatingServer/server.js
+++ CreatingServer/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
 
-//ser header content type
+//set header content type
 res.setHeader('Content-Type', 'text/html');
 
 res.write('<p>Hello, Allan</p>');
