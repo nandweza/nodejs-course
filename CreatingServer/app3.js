abd6f0919bbfc6c75dcee0bf6234030c2dd615b4
nandweza <allannandweza@gmail.com>
2022-05-18 11:27:26 +0300

    middleware and static files

---
+++ CreatingServer/app3.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const morgan = require('morgan');
+
+//express app
+const app = express();
+
+//register view engine
+app.set('view engine', 'ejs');
+
+//listen for requests.
+app.listen(3000);
+
+//middleware & static files.
+app.use(express.static('public'));
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+    const blogs = [
+        {title: 'Yoshi and ninjas', snippet: 'djbfdsbfbidsiaidsgudhjncvnvifufidnckn'},
+        {title: 'ninja swords', snippet: 'djbfdsbfbidsiaidsgudhjncvnvifufidnckn'},
+        {title: 'Yoshi and yakuza', snippet: 'djbfdsbfbidsiaidsgudhjncvnvifufidnckn'}
+    ];
+    res.render('index', { title: 'Home', blogs });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'About page' });
+});
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create a new blog' });
+});
+
+//404 page
+app.use((req, res) => {
+    res.status(404).render('404', { title: '404' });
+});
